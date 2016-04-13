aacdfd74ceae92c06caedb136b8bbae8dcd88ca0
Fukuball Lin <fukuball@gmail.com>
2016-04-13 14:39:39 +0800

    Add ridge regression binary classification

--- FukuML/RidgeRegression.py
+++ FukuML/RidgeRegression.py
@@ -24,6 +24,8 @@ class RidgeRegression(ml.Learner):
         self.feature_transform_mode = ''
         self.feature_transform_degree = 1
 
+        self.lambda_p = 0.0001
+
     def load_train_data(self, input_data_file=''):
 
         self.status = 'load_train_data'
@@ -61,6 +63,12 @@ class RidgeRegression(ml.Learner):
 
         return self.test_X, self.test_Y
 
+    def setLambda(self, lambda_p=0.0001):
+
+        self.lambda_p = lambda_p
+
+        return self.lambda_p
+
     def init_W(self):
 
         if (self.status != 'load_train_data') and (self.status != 'train'):
@@ -95,7 +103,7 @@ class RidgeRegression(ml.Learner):
 
         return super(RidgeRegression, self).calculate_avg_error()
 
-    def train(self, lambda_p=0.0001):
+    def train(self):
 
         if (self.status != 'init'):
             print("Please load train data and init W first.")
@@ -103,7 +111,7 @@ class RidgeRegression(ml.Learner):
 
         self.status = 'train'
 
-        inverse_part = np.linalg.inv(np.dot(self.train_X.transpose(), self.train_X) + lambda_p * np.eye(self.train_X.shape[1]))
+        inverse_part = np.linalg.inv(np.dot(self.train_X.transpose(), self.train_X) + self.lambda_p * np.eye(self.train_X.shape[1]))
         self.W = np.dot(np.dot(inverse_part, self.train_X.transpose()), self.train_Y)
 
         return self.W
@@ -113,7 +121,7 @@ class RidgeRegression(ml.Learner):
         return super(RidgeRegression, self).prediction(input_data, mode)
 
 
-class BinaryClassifier(ml.Learner):
+class BinaryClassifier(RidgeRegression):
 
     def __init__(self):
 
@@ -130,20 +138,78 @@ class BinaryClassifier(ml.Learner):
         self.feature_transform_mode = ''
         self.feature_transform_degree = 1
 
+        self.lambda_p = 0.0001
+
     def load_train_data(self, input_data_file=''):
-        return
+
+        self.status = 'load_train_data'
+
+        if (input_data_file == ''):
+            input_data_file = os.path.normpath(os.path.join(os.path.join(os.getcwd(), os.path.dirname(__file__)), "dataset/ridge_regression_train.dat"))
+        else:
+            if (os.path.isfile(input_data_file) is not True):
+                print("Please make sure input_data_file path is correct.")
+                return self.train_X, self.train_Y
+
+        self.train_X, self.train_Y = utility.DatasetLoader.load(input_data_file)
+
+        return self.train_X, self.train_Y
 
     def load_test_data(self, input_data_file=''):
-        return
+
+        if (input_data_file == ''):
+            input_data_file = os.path.normpath(os.path.join(os.path.join(os.getcwd(), os.path.dirname(__file__)), "dataset/ridge_regression_test.dat"))
+        else:
+            if (os.path.isfile(input_data_file) is not True):
+                print("Please make sure input_data_file path is correct.")
+                return self.test_X, self.test_Y
+
+        self.test_X, self.test_Y = utility.DatasetLoader.load(input_data_file)
+
+        if (self.feature_transform_mode == 'polynomial') or (self.feature_transform_mode == 'legendre'):
+            self.test_X = self.test_X[:, 1:]
+
+            self.test_X = utility.DatasetLoader.featureTransform(
+                self.test_X,
+                self.feature_transform_mode,
+                self.feature_transform_degree
+            )
+
+        return self.test_X, self.test_Y
+
+    def setLambda(self, lambda_p=0.0001):
+
+        return super(BinaryClassifier, self).setLambda(lambda_p)
 
     def init_W(self):
-        return
+
+        return super(BinaryClassifier, self).init_W()
 
     def score_function(self, x, W):
-        return
+
+        score = np.sign(np.inner(x, W))
+
+        return score
 
     def error_function(self, y_prediction, y_truth):
-        return
+
+        if y_prediction != y_truth:
+            return 1
+        else:
+            return 0
+
+    def calculate_avg_error(self, X, Y, W):
+
+        return super(BinaryClassifier, self).calculate_avg_error(X, Y, W)
+
+    def calculate_test_data_avg_error(self):
+
+        return super(BinaryClassifier, self).calculate_avg_error()
 
     def train(self):
-        return
+
+        return super(BinaryClassifier, self).train()
+
+    def prediction(self, input_data='', mode='test_data'):
+
+        return super(BinaryClassifier, self).prediction(input_data, mode)
--- test_fuku_ml.py
+++ test_fuku_ml.py
@@ -1042,6 +1042,7 @@ class FukuMLTestCase(unittest.TestCase):
         ridge = ridge_regression.RidgeRegression()
         ridge.load_train_data()
         ridge.load_test_data()
+        ridge.setLambda(pow(10, -3))
         ridge.init_W()
         W = ridge.train()
 
@@ -1063,6 +1064,56 @@ class FukuMLTestCase(unittest.TestCase):
         print(ridge.calculate_avg_error(ridge.train_X, ridge.train_Y, W))
         print("W 平均錯誤值（Eout）：")
         print(ridge.calculate_avg_error(ridge.test_X, ridge.test_Y, W))
+        print('-'*70)
+
+    def test_ridge_regression_binary_classifier(self):
+
+        #------------------------------------------------------------
+
+        cross_validator = utility.CrossValidator()
+
+        lambda_p_set = [i for i in range(2, -11, -1)]
+
+        for lambda_p in lambda_p_set:
+            ridge_bc = ridge_regression.BinaryClassifier()
+            ridge_bc.load_train_data()
+            ridge_bc.setLambda(pow(10, lambda_p))
+            cross_validator.addModel(ridge_bc)
+
+        print("\n10 fold cross validation：")
+        avg_errors = cross_validator.excute()
+
+        print("\n各模型驗證平均錯誤：")
+        print(avg_errors)
+        print("\n最小平均錯誤率：")
+        print(cross_validator.getMinAVGError())
+
+        print("\n取得最佳模型：")
+        best_model = cross_validator.getBestModel()
+        print(best_model)
+
+        best_model.load_test_data()
+        best_model.init_W()
+        W = best_model.train()
+
+        print("\n訓練得出權重模型：")
+        print(W)
+
+        test_data = '0.402041 0.402048 -1'
+        prediction = best_model.prediction(test_data)
+
+        self.assertEqual(prediction['input_data_y'], prediction['prediction'])
+
+        print("測試資料 x：")
+        print(prediction['input_data_x'])
+        print("測試資料 y：")
+        print(prediction['input_data_y'])
+        print("預測結果：")
+        print(prediction['prediction'])
+        print("W 平均錯誤率（Ein）：")
+        print(best_model.calculate_avg_error(best_model.train_X, best_model.train_Y, W))
+        print("W 平均錯誤率（Eout）：")
+        print(best_model.calculate_avg_error(best_model.test_X, best_model.test_Y, W))
         print('-'*70)
 
 if __name__ == '__main__':
