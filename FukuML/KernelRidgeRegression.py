c0bc0683f179db08fa962fcf80ecd568ebb83d66
Fukuball Lin <fukuball@gmail.com>
2016-06-16 01:50:14 +0800

    Add Kernel Ridge Regression Binary Classifier

--- FukuML/KernelRidgeRegression.py
+++ FukuML/KernelRidgeRegression.py
@@ -95,3 +95,75 @@ class KernelRidgeRegression(ridge_regression.RidgeRegression):
     def prediction(self, input_data='', mode='test_data'):
 
         return super(KernelRidgeRegression, self).prediction(input_data, mode)
+
+
+class BinaryClassifier(KernelRidgeRegression):
+
+    def __init__(self):
+
+        """init"""
+
+        self.status = 'empty'
+        self.train_X = []
+        self.train_Y = []
+        self.W = []
+        self.data_num = 0
+        self.data_demension = 0
+        self.test_X = []
+        self.test_Y = []
+        self.feature_transform_mode = ''
+        self.feature_transform_degree = 1
+
+        self.lambda_p = 0.0001
+        self.svm_kernel = 'soft_gaussian_kernel'
+        self.zeta = 0
+        self.gamma = 1
+        self.Q = 1
+        self.C = 0.1
+        self.beta = []
+
+    def load_train_data(self, input_data_file=''):
+
+        return super(BinaryClassifier, self).load_train_data(input_data_file)
+
+    def load_test_data(self, input_data_file=''):
+
+        return super(BinaryClassifier, self).load_test_data(input_data_file)
+
+    def set_param(self, lambda_p=0.0001, gamma=1, C=0.1):
+
+        return super(BinaryClassifier, self).set_param(lambda_p, gamma, C)
+
+    def init_W(self):
+
+        return super(BinaryClassifier, self).init_W()
+
+    def score_function(self, x, W):
+
+        score = super(BinaryClassifier, self).score_function(x, W)
+        score = np.sign(score)
+
+        return score
+
+    def error_function(self, y_prediction, y_truth):
+
+        if y_prediction != y_truth:
+            return 1
+        else:
+            return 0
+
+    def calculate_avg_error(self, X, Y, W):
+
+        return super(BinaryClassifier, self).calculate_avg_error(X, Y, W)
+
+    def calculate_test_data_avg_error(self):
+
+        return super(BinaryClassifier, self).calculate_test_data_avg_error()
+
+    def train(self):
+
+        return super(BinaryClassifier, self).train()
+
+    def prediction(self, input_data='', mode='test_data'):
+
+        return super(BinaryClassifier, self).prediction(input_data, mode)
--- test_fuku_ml.py
+++ test_fuku_ml.py
@@ -1823,7 +1823,7 @@ class FukuMLTestCase(unittest.TestCase):
         kernel_ridge = kernel_ridge_regression.KernelRidgeRegression()
         kernel_ridge.load_train_data()
         kernel_ridge.load_test_data()
-        kernel_ridge.set_param(lambda_p=pow(10, -3))
+        kernel_ridge.set_param(lambda_p=pow(10, -3), gamma=1, C=0.1)
         kernel_ridge.init_W()
         kernel_ridge.train()
 
@@ -1845,6 +1845,37 @@ class FukuMLTestCase(unittest.TestCase):
         print(kernel_ridge.calculate_avg_error(kernel_ridge.train_X, kernel_ridge.train_Y, kernel_ridge.W))
         print("W 平均錯誤值（Eout）：")
         print(kernel_ridge.calculate_avg_error(kernel_ridge.test_X, kernel_ridge.test_Y, kernel_ridge.W))
+        print('-'*70)
+
+    def test_kernel_ridge_regression_binary_classifier(self):
+
+        #------------------------------------------------------------
+
+        kernel_ridge_b = kernel_ridge_regression.BinaryClassifier()
+        kernel_ridge_b.load_train_data()
+        kernel_ridge_b.load_test_data()
+        kernel_ridge_b.set_param(lambda_p=pow(10, -3), gamma=1, C=0.1)
+        kernel_ridge_b.init_W()
+        kernel_ridge_b.train()
+
+        print("\n訓練得出權重模型：")
+        print(kernel_ridge_b.beta)
+
+        test_data = '0.62771 0.11513 0.82235 0.14493 -1'
+        prediction = kernel_ridge_b.prediction(test_data)
+
+        print("測試資料 x：")
+        print(prediction['input_data_x'])
+        print("測試資料 y：")
+        print(prediction['input_data_y'])
+        print("預測結果：")
+        print(prediction['prediction'])
+        print("錯誤評估：")
+        print(kernel_ridge_b.error_function(prediction['prediction'], prediction['input_data_y']))
+        print("W 平均錯誤值（Ein）：")
+        print(kernel_ridge_b.calculate_avg_error(kernel_ridge_b.train_X, kernel_ridge_b.train_Y, kernel_ridge_b.W))
+        print("W 平均錯誤值（Eout）：")
+        print(kernel_ridge_b.calculate_avg_error(kernel_ridge_b.test_X, kernel_ridge_b.test_Y, kernel_ridge_b.W))
         print('-'*70)
 
 if __name__ == '__main__':
